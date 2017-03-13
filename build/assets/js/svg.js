2955e616a395bb2c23830dd9c8a5a60f5c5346c7
smit thakkar <smitthakkar96@gmail.com>
2017-03-13 21:50:14 +0530

    add build folder to gitignore, removed build folder

--- build/assets/js/svg.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('svg').addEventListener('click', function() {
-    document.querySelector('svg').classList.toggle('animated')
-    console.log("fuck")
-})
-$("svg").click(function() {
-    this.addclass("animated")
-})
